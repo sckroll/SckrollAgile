5325b9373248325a0343befec6dde7c6e8c374d2
sckroll <kimsc0714@gmail.com>
2020-06-11 21:24:29 +0900

    create back-end scaffold

---
+++ src/test/java/com/sckroll/sckrollagile/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sckroll.sckrollagile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
