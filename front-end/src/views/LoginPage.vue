2c4d772208431559fee72e4e5072b038e696b18d
sckroll <kimsc0714@gmail.com>
2020-06-17 02:17:45 +0900

    integrate front-end & back-end build

---
+++ front-end/src/views/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>SckrollAgile</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ front-end/src/views/RegisterPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>asdf</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
