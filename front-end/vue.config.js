e2803863888c9716488eb850c1ce2111ea264593
sckroll <kimsc0714@gmail.com>
2020-06-17 03:40:05 +0900

    add http proxy config

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   devServer: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      // '/api/' 경로로 시작하는 요청은 백엔드로 연결
+      '/api/*': {
+        target: 'http://localhost:8080'
+      }
+    }
   }
 }
